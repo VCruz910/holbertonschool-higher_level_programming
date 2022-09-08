f7ec940040da9b746ca5bace88ba2c4c2bae61be
VCruz910 <vscruz910@gmail.com>
2022-09-08 21:44:29 +0000

    Made program that prints in uppercases (fix 001)

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for Char in range(len(str)):
-        if ord(str[Char]) >= 97 and ord(str[Char]) <=122:
+        if ord(str[Char]) >= 97 and ord(str[Char]) <= 122:
             Num = 32
         else:
             Num = 0
