9b17ad1664b2a4d4cd63597d314941ed343ad018
VCruz910 <vscruz910@gmail.com>
2022-09-08 22:28:47 +0000

    Made fizzbuzz, but in python

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for Num in range(1, 101):
+        if Num % 3 == 0 and Num % 5 == 0:
+            print("FizzBuzz", end='')
+        elif Num % 3 == 0:
+            print("Fizz", end='')
+        elif Num % 5 == 0:
+            print("Buzz", end='')
+        else:
+            print(Num, end='')
+        print(" ", end='')
