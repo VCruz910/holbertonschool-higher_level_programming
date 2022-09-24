fac7ee87c0269997cb87c783f59c805e3dd24be5
VCruz910 <vscruz910@gmail.com>
2022-09-24 21:59:54 +0000

    Fixed file documentation to meet standards(take 002

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 class Square:
     """
-    represent a square.
-    Private Instance Attribute: size.
-    Instantiation with optional size.
+        Represent a square.
+        Private Instance Attribute: size.
+        Instantiation with optional size.
     """
 
     def __init__(self, size=0):
-        """Initializes the data."""
+        """
+            Initializes the data.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
