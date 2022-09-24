35a06d290d0ef8d74271c3640d62e7298ba87bb1
VCruz910 <vscruz910@gmail.com>
2022-09-24 21:20:26 +0000

    Created class that defines a square size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """Class that represents a square.
+    Private Instance Attribute: size.
+    Instantiation with size (no type/value verification)
+    """
+    def __init__(self, size):
+        """Initializes the data for storing the square's size.
+        Args:
+            param1 (int): Size of the square.
+        """
+        self.__size = size
