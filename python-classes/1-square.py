1b1df8d6b0b5918027bd468c3799e24e9acb5133
VCruz910 <vscruz910@gmail.com>
2022-09-25 19:50:47 +0000

    Fixed file documentation to meet standards(take 003)

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""Created an empty class that defines our square."""
 class Square:
-    """Class that represents a square.
+    """
+    Class that represents a square.
+
     Private Instance Attribute: size.
     Instantiation with size (no type/value verification)
     """
     def __init__(self, size):
-        """Initializes the data for storing the square's size.
+        """
+        Initializes the data for storing the square's size.
+        
         Args:
             param1 (int): Size of the square.
         """
