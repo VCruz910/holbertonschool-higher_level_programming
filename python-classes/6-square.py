d6afd173b4986977195c32067d9a2916c01f90e9
VCruz910 <vscruz910@gmail.com>
2022-09-25 22:01:40 +0000

    Fixing code errors (take 003)

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -48,7 +48,7 @@ class Square:
         """Sets the position of a square to a value."""
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive")
-        if not isinstance(value[0], int) or not isinstance(value[1],int):
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
             raise TypeError("position must be a tuple of 2 positive")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive")
