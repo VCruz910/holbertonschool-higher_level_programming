3246c79491e7ac6db6c3f2b99c3179f5427bc180
VCruz910 <vscruz910@gmail.com>
2022-09-25 19:21:25 +0000

    Created class that defines a square based on 2-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class Square:
+    """Class that represents a square.
+
+    Private Instance Attribute: size.
+    Instantiation with optional size.
+    Public Instance Method: def area(self).
+    """
+
+    def __init__(self, size=0):
+        """Initialiazes the data."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the current square's area."""
+        return self.__size ** 2
