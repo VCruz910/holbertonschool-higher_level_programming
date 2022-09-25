ec0b128eb1a610e4c9bbcb85ff1698ed61d417d1
VCruz910 <vscruz910@gmail.com>
2022-09-25 21:31:03 +0000

    Fixing code errors (take 002)

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -37,6 +37,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     @property
     def position(self):
         """Retrieves the position of the square."""
@@ -45,18 +46,17 @@ class Square:
     @position.setter
     def position(self, value):
         """Sets the position of a square to a value."""
-        if not isinstance(value, tuple) or len (value) != 2:
+        if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive")
         if not isinstance(value[0], int) or not isinstance(value[1],int):
             raise TypeError("position must be a tuple of 2 positive")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive")
-        else:
-            self.__position = value
+        self.__position = value
 
     def area(self):
         """Returs the area of the current square."""
-        return self.__size **2
+        return self.__size ** 2
 
     def my_print(self):
         """
@@ -65,12 +65,12 @@ class Square:
         """
         if self.__size == 0:
             print()
-            return
-        for Y in range(0, self.__position[1]):
-            print()
-        for I in range(0, self.__size):
-            for X in range(0, self.position[0]):
-                print(" ", end="")
-            for J in range(0, self.__size):
-                print("#", end="")
-            print()
+        else:
+            for Y in range(0, self.__position[1]):
+                print()
+            for I in range(0, self.__size):
+                for X in range(0, self.position[0]):
+                    print(" ", end="")
+                for J in range(0, self.__size):
+                    print("#", end="")
+                print()
