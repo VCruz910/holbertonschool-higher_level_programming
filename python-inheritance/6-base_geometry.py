45173a92f743d24b3e74e64212981241c277af87
VCruz910 <vscruz910@gmail.com>
2022-09-29 21:52:19 +0000

    Created Class based on 5-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module: 6-base_geometry
+
+Class that handles
+geometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Handles Geometry
+
+    Attributes:
+    ====================
+    Public Instance Method: def area(self).
+    """
+
+    def area(self):
+        """
+        Defines the Area.
+
+        Arguments:
+            - None.
+        Returns:
+            - None.
+        Raises:
+            - Exception:
+                * Raises an exception with the
+                message 'area() is not implemented'.
+        """
+        raise Exception("area() is not implemented")
