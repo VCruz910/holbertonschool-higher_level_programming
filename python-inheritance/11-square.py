976006cd486b6eaa3da418355197fe7f86c993cb
VCruz910 <vscruz910@gmail.com>
2022-09-30 00:12:17 +0000

    Created Class Rectangle that inherits from BaseGeometry based on 10-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Defines a Square from Rectangle class.
+    """
+    def __init__(self, size):
+        """
+        Initializes a Square.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """
+        Returns a string with the area.
+        """
+        return super().area()
+
+    def __str__(self):
+        """
+        Returns a printable string.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
