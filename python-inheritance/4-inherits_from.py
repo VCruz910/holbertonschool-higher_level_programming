a16af1ecf409b0c22c9ae32638dcf2f713624c5c
VCruz910 <vscruz910@gmail.com>
2022-09-30 17:45:27 +0000

    Fixed Documentation for this function

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
-"""
-Module: 4-inherits_from
-
-Function that returns 'True'
-if the object is an instance
-of a class that inherited
-(directly or indirectly)
-from the specified class;
-otherwise False.
-"""
+"""Checks if an object is inherited"""
 
 
 def inherits_from(obj, a_class):
-    """
-    Checks if the object is an
-    instance inherited.
-    """
+    """Checks if the object is an instance inherited."""
     if type(obj) is a_class:
         return False
     return isinstance(obj, a_class)
