1908d1730d6b68334f4b25efba168723c3487e9f
VCruz910 <vscruz910@gmail.com>
2022-09-30 17:39:54 +0000

    Fixed Documentation for this function (take???)

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,25 +2,12 @@
 """
 Module: 1-my_list
 Class that inherits
-from 'list'.
+from list.
 """
 
 
 class MyList(list):
-    """
-    Class for organizing
-    lists.
-
-    Arguments:
-        - list: Class List
-    Attributes:
-    ====================
-    Public Instance Method:
-        - def print_sorted(self):
-    """
+    """Class for organizing lists."""
     def print_sorted(self):
-        """
-        Prints our list in
-        ascending order.
-        """
+        """Prints our list in ascending order."""
         print(sorted(self))
