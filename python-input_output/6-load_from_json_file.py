17adb9c5936b7f9d4dbda1b0c8e0ba86d5eac446
VCruz910 <vscruz910@gmail.com>
2022-10-01 00:34:58 +0000

    Wrote function that creates an object from JSON

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Creates an Object from a "JSON file"."""
+
+
+def load_from_json_file(filename):
+    """Returns corresponding Python object."""
+    with open(filename, 'r') as f:
+        return json.loads(f.read())
