56c0b4cb963a32b0eb4964cdd0ed171208af808a
VCruz910 <vscruz910@gmail.com>
2022-10-01 01:45:30 +0000

    Wrote class that defines a student

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -3,7 +3,13 @@
 
 
 class Student:
-    """Defines a student."""
+    """Class that defines a student.
+    Public attributes:
+        - first_name
+        - last_name
+        - age
+    Public method to_json().
+    """
 
     def __init__(self, first_name, last_name, age):
         """Initializes the Student instance."""
@@ -12,14 +18,18 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Retrieves a dictionary representation of a Student instance."""
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation
+        of a Student instance.
+        Args:
+            - attrs: list of attributes
+        Returns: the dict representation of the instance.
+        """
 
-         my_dict = dict()
-
+        my_dict = dict()
         if type(attrs) is list and all(type(x) is str for x in attrs):
             for x in attrs:
                 if x in self.__dict__:
                     my_dict.update({x: self.__dict__[x]})
             return my_dict
-        return self.__dict__
+        return self.__dict__.copy()
