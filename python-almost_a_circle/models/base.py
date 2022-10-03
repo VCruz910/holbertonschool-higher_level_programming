3810410a24e6d2d5f67f00314b5a401a50b3aa84
VCruz910 <vscruz910@gmail.com>
2022-10-03 18:34:25 +0000

    Created class for all classes

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Defines a Base Class for the other classes
+in project.
+"""
+
+#imports
+import json
+import csv
+import os
+
+
+class Base:
+    """
+    Base Class for the other classes.
+
+    Attributes:
+    ======================================
+    Private Class Attribute: __nb_objects.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Base Class Initialization.
+
+        Arguments:
+            - id: Instance ID.
+        Returns:
+            - None.
+        Raises:
+            - TypeError:
+                * If id is not an integer.
+        """
+        if type(id) != int and id is not None:
+            raise TypeError("ID must be an integer")
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
