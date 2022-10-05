504468042c52efd82280494173351b8525b6fdf0
VCruz910 <vscruz910@gmail.com>
2022-10-05 15:42:14 +0000

    Fixed indentation in line 255

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -251,6 +251,5 @@ class Rectangle(Base):
         Returns the dictionary representation
         of a Rectangle.
         """
-        MyDICT = {'id': self.id, 'width': self.width, 'height': self.height,
-                'x': self.x, 'y': self.y}
+        MyDICT = {'id': self.id, 'width': self.width, 'height': self.height, 'x': self.x, 'y': self.y}
         return (MyDICT)
