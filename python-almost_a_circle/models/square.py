b5659a69132677d0c51930c46665b9f6187a8e5e
VCruz910 <vscruz910@gmail.com>
2022-10-06 21:26:06 +0000

    Fixing error message(Changed size to width)

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -56,9 +56,9 @@ class Square(Rectangle):
         """
 
         if type(value) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("size must be > 0")
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
