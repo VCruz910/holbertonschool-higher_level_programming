f9753be0863e632f9823bcdb009c7de4c55891a2
VCruz910 <vscruz910@gmail.com>
2022-10-07 13:27:32 +0000

    Fixing indentaion on line 255

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -252,5 +252,5 @@ class Rectangle(Base):
         of a Rectangle.
         """
         MyDICT = {'id': self.id, 'width': self.__width,
-                'height': self.__height, 'x': self.__x, 'y': self.__y}
+        'height': self.__height, 'x': self.__x, 'y': self.__y}
         return (MyDICT)
