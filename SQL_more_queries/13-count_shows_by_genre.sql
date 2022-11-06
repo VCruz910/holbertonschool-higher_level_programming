28fcdd2e0b7c3ceee0d461f10f798d0b9b557697
VCruz910 <vscruz910@gmail.com>
2022-11-06 21:13:20 +0000

    Wrote scripts imports database dumps to a server

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Number of shows by genre
 -- Imports database dump
-SELECT tv_shows.title AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
-FROM tv_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_show_genres
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC, tv_genres.id ASC;
