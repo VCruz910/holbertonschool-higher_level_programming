fe47e73f45a80f30c84ec90dd32cb146ab1edd75
VCruz910 <vscruz910@gmail.com>
2022-11-06 20:40:30 +0000

    Wrote scripts that creates and lists tables and databases

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,9 +2,11 @@
 -- Creates a database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
+(
   id INT UNIQUE AUTO_IMCREMENT NOT NULL,
   state_id INT NOT NULL,
   name VARCHAR(256) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (state_id) REFERENCES states(id));
+  FOREIGN KEY (state_id) REFERENCES states(id)
+);
