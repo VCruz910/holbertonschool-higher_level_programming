20f19ceed5eb2e24531d4083c28c262a46a6383e
VCruz910 <vscruz910@gmail.com>
2022-11-08 20:12:44 +0000

    Fixing errors (Take 002)

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,17 +3,14 @@
 Lists all states from
 hbtn_0e_0_usa database.
 """
-
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    DB = MYSQLdb.connect(user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3],
-            port=3306)
-    CURSOR = DB.CURSOR()
-    CURSOR.execute("SELECT * FROM states;")
-    states = CURSOR.fetchall()
+    DB = MYSQLdb.connect(user=sys.argv[1],passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    DBCur = DB.cursor()
+    DBCur.execute("SELECT * FROM states;")
+    states = DBCur.fetchall()
 
     for state in states:
         print(state)
