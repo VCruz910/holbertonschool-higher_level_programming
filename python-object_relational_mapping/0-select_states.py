c20315093e935c53be4bfb42752ed10acac39f69
VCruz910 <vscruz910@gmail.com>
2022-11-08 20:14:59 +0000

    Fixing errors (Take 004)

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    DB = MYSQLdb.connect(user=sys.argv[1],passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    DB = MySQLdb.connect(user=sys.argv[1],passwd=sys.argv[2], db=sys.argv[3], port=3306)
     DBCur = DB.cursor()
     DBCur.execute("SELECT * FROM states;")
     states = DBCur.fetchall()
