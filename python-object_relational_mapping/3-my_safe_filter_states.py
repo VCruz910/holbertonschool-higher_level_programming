3a9d2e1c6f7e3c294787d316ae1dc4009c6e8ba5
VCruz910 <vscruz910@gmail.com>
2022-11-08 21:20:37 +0000

    Wrote script that takes in an argument and displays values(safe from SQL Injections version)

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
 """
-Lists all states from hbtn_0e_0_usa database.
+Lists all values in the states tables of a database where name
+matches the argument in a safe way
 """
-
-
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    DB = MySQLdb.connect(
+    db = MySQLdb.connect(
             host="localhost",
             port=3306,
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3])
-    DBCur = DB.cursor()
-    DBCur.execute(
-            "SELECT * FROM states WHERE name LIKE %s
-            ORDER BY id ASC;", (sys.argv[4]))
-    states = DBCur.fetchall()
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
+    states = cur.fetchall()
 
     for state in states:
         print(state)
