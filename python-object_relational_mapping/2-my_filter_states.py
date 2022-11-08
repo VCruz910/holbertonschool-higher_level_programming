4d748f3a16c14d1711d8375ae413dfc1cfbd02d7
VCruz910 <vscruz910@gmail.com>
2022-11-08 21:04:27 +0000

    Wrote script that takes in an argument and displays values

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-
-
-'''
-Takes in an argument and
-displays all values in state
-table of hbtn_0e_0_usa where
-name matches arguments.
-'''
+"""
+Lists all values in the states tables of a database where name
+matches the argument
+"""
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    DB = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
+    db = MySQLdb.connect(host="localhost", port=3306,
+            user=sys.argv[1], passwd=sys.argv[2],
             db=sys.argv[3])
-    DBCur = DB.cursor()
-    DBCur.execute(
-            "SELECT * FROM states WHERE name LIKE " "
-            '{:s}' ORDER BY id ASC;".format(sys.argv[4]))
-    states = DBCur.fetchall()
+
+    cur = db.cursor()
+    cur.execute("SELECT * \
+            FROM states \
+            WHERE CONVERT(`name` USING Latin1) \
+            COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
 
     for state in states:
         print(state)
