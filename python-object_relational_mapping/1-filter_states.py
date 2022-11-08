1cb3881855115b3ba1fb5d603d332272e01f1468
VCruz910 <vscruz910@gmail.com>
2022-11-08 20:26:27 +0000

    Wrote script that list all states with a name starting with uppercase N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Lists all states with name
+starting with uppercase N
+from hbtn_0e_0_usa database.
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    DB = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306)
+
+    DBCur = DB.cursor()
+    DBCur.execute("SELECT * \
+            FROM states \
+            WHERE CONVERT('name' USING Latin1) \
+            COLLATE Latin1_General_CS \
+            LIKE 'N%';")
+    states = DBCur.fetchall()
+
+    for state in states:
+        print(state)
