3f2a6d65dc5bca5c5df88447c08600c37ff59123
VCruz910 <vscruz910@gmail.com>
2022-11-08 20:16:55 +0000

    Fixing for pycodestyle validation (take 001)

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,10 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    DB = MySQLdb.connect(user=sys.argv[1],passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    DB = MySQLdb.connect(user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306)
     DBCur = DB.cursor()
     DBCur.execute("SELECT * FROM states;")
     states = DBCur.fetchall()
