0289c8210981a50d97d9fcb00355242f436a59d6
VCruz910 <vscruz910@gmail.com>
2022-11-08 20:46:15 +0000

    Fixing errors (take 002)

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,15 +8,15 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    DB = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-            db=sys.argv[3], port=3306)
-
+    DB = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
     DBCur = DB.cursor()
-    DBCur.execute("SELECT * \
-            FROM states \
-            WHERE CONVERT('name' USING Latin1) \
-            COLLATE Latin1_General_CS \
-            LIKE 'N%';")
+    DBCur.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
     states = DBCur.fetchall()
 
     for state in states:
