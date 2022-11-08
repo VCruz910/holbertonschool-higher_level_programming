93f6faa63e2d545b7ba7bed16a57018ecc630714
VCruz910 <vscruz910@gmail.com>
2022-11-08 20:56:48 +0000

    Wrote script that takes in an argument and displays values

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Takes in an argument and
+displays all values is state
+table of hbtn_0e_0_usa where
+name matches arguments.
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    DB = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
+    DBCur = DB.cursor()
+    DBCur.execute(
+            "SELECT * FROM states WHERE name LIKE " "
+            '{:s}' ORDER BY id ASC;".format(sys.argv[4]))
+    states = DBCur.fetchall()
+
+    for state in states:
+        print(state)
