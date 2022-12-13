a41fe1f10ca38f6222a73a151155d91c9ee4a95d
VCruz910 <vscruz910@gmail.com>
2022-12-13 16:30:05 -0400

    Created square class using Rectangle class attributes

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,18 +2,18 @@
 const SquareP = require('./5-square');
 
 class Square extends SquareP {
-    charPrint(c) {
-        if (c === undefined) {
-            c = 'X';
-        }
-        for (let I = 0; I < this.height; I++) {
-            let S = '';
-            for (let J = 0; J < this.width; J++) {
-                S += c;
-            }
-            console.log(S);
-        }
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
+    for (let I = 0; I < this.height; I++) {
+      let S = '';
+      for (let J = 0; J < this.width; J++) {
+        S += c;
+      }
+      console.log(S);
+    }
+  }
 }
 
 module.exports = Square;
