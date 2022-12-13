112f0e04fbe57f9f49006b7ae6b79393dc102e5d
VCruz910 <vscruz910@gmail.com>
2022-12-13 16:31:28 -0400

    Changed height to width on secon for in print to fix code

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,32 +1,32 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(W_Rec, H_Rec) {
-        if ((W_Rec > 0) && (H_Rec > 0)) {
-            this.width = W_Rec;
-            this.height = H_Rec;
-        }
+  constructor (W_Rec, H_Rec) {
+    if ((W_Rec > 0) && (H_Rec > 0)) {
+      this.width = W_Rec;
+      this.height = H_Rec;
     }
+  }
 
-    print() {
-        for (let I = 0; I < this.height; I++) {
-            let S = '';
-            for (let J = 0; J < this.width; J++) {
-                S += 'X';
-            }
-            console.log(S);
-        }
+  print () {
+    for (let I = 0; I < this.height; I++) {
+      let S = '';
+      for (let J = 0; J < this.width; J++) {
+        S += 'X';
+      }
+      console.log(S);
     }
+  }
 
-    rotate() {
-        const AUX = this.width;
-        this.width = this.height;
-        this.height = AUX;
-    }
+  rotate () {
+    const AUX = this.width;
+    this.width = this.height;
+    this.height = AUX;
+  }
 
-    double() {
-        this.width *= 2;
-        this.height *= 2;
-    }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
